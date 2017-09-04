2924a1eb3b765c5837a69bfaf4bb40e01e9b56b1
Satria Prayudi <satryacode@gmail.com>
2017-09-04 13:17:37 +0700

    Update MockLocation.java
    add detection for android.os.Build.VERSION.SDK_INT >= 18

--- src/android/com/kosh/MockLocation.java
+++ src/android/com/kosh/MockLocation.java
@@ -3,19 +3,27 @@ package com.kosh;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.provider.Settings.Secure;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+// import android.provider.Settings.Secure;
 
 public class MockLocation extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext, Location location) throws JSONException {
         if (action.equals("check")) {
-			if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
-				callbackContext.success("false");
-			} else {
-				callbackContext.success("true");
-			}
+			// if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+			// 	callbackContext.success("false");
+			// } else {
+			// 	callbackContext.success("true");
+			// }
+	    //android.os.Build.VERSION.SDK_INT >= 18
+            if(location.isFromMockProvider()){
+                callbackContext.success("true");
+            } else {
+                callbackContext.success("false");
+            }
             return true;
         } else {
             return false;
